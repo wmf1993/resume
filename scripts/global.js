fd17bf66d9845d9025d250deb5f1307f0597e0f9
Mingfei Wu <31245615+wmf1993@users.noreply.github.com>
2018-09-12 22:37:13 +0800

    change
    change

---
+++ scripts/global.js
@@ -0,0 +1,30 @@
+(function () {
+  var isMobile = false;  // PC  
+  function mobile() {
+    try {
+      document.createEvent('TouchEvent');
+      return true;  // mobile
+    }
+    catch (e) {
+      return false;
+    }
+  }
+  isMobile = mobile();
+  
+  var links = document.getElementsByTagName('link');
+  if (isMobile == true) {
+    for (var i=0; i<links.length; i++) {
+      var linkHref = links[i].href;
+      if ((/(.*)(main.css)/ig).test(linkHref) == false) continue;
+      links[i].href = linkHref.replace(/(.*)(main.css)/ig, '$1'+'mobile.css');
+      var meta = document.createElement('meta');
+      meta.name = 'viewport';
+      meta.content = 'width=device-width';
+      console.log(meta);
+      links[i].parentNode.appendChild(meta);
+    }
+  } else {
+    return;
+  }
+})();
+      

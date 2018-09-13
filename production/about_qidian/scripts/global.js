2310991db2effadfef58bfbc066350ecc82e8fab
Mingfei Wu <31245615+wmf1993@users.noreply.github.com>
2018-09-13 23:55:45 +0800

    qidian homepage
    continue to practice

---
+++ production/about_qidian/scripts/global.js
@@ -0,0 +1,20 @@
+function browserChange() {
+  var width = window.innerWidth;
+  var links = document.getElementsByTagName('link');
+  if (width < 1200) {
+    for (var i=0; i<links.length; i++) {
+      var cssHref = links[i].href;
+      if (/(.*)(main.css)/ig.test(cssHref) == true) {
+        links[i].href = cssHref.replace(/(.*)(main.css)/ig, '$1'+'main-small.css');
+      }
+    }
+  } else {
+    for (var i=0; i<links.length; i++) {
+      var cssHref = links[i].href;
+      if (/(.*)(main-small.css)/ig.test(cssHref) == true) {
+        links[i].href = cssHref.replace(/(.*)(main-small.css)/ig, '$1'+'main.css');
+      }
+    }
+  }
+}
+window.onresize = browserChange;

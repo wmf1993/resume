533d2f6a99487f2a4439dcae095751c3827f429b
Mingfei Wu <31245615+wmf1993@users.noreply.github.com>
2018-09-17 01:00:04 +0800

    continue
    continue

--- production/about_qidian/scripts/global.js
+++ production/about_qidian/scripts/global.js
@@ -1,19 +1,33 @@
+// onload handler to bind multiple directives
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
 // Adapt to browser width
+// change_1: 统一共同部分的CSS，浏览器宽度应用相应的CSS(-big/-small)
 function browserChange() {
   var width = window.innerWidth;
   var links = document.getElementsByTagName('link');
   if (width < 1200) {
     for (var i=0; i<links.length; i++) {
       var cssHref = links[i].href;
-      if (/(.*)(main.css)/ig.test(cssHref) == true) {
-        links[i].href = cssHref.replace(/(.*)(main.css)/ig, '$1'+'main-small.css');
+      if (/(.*)(main-big.css)/ig.test(cssHref) == true) {
+        links[i].href = cssHref.replace(/(.*)(main-big.css)/ig, '$1'+'main-small.css');
       }
     }
   } else {
     for (var i=0; i<links.length; i++) {
       var cssHref = links[i].href;
       if (/(.*)(main-small.css)/ig.test(cssHref) == true) {
-        links[i].href = cssHref.replace(/(.*)(main-small.css)/ig, '$1'+'main.css');
+        links[i].href = cssHref.replace(/(.*)(main-small.css)/ig, '$1'+'main-big.css');
       }
     }
   }
@@ -44,10 +58,52 @@ function closeClick() {
       var childs = that.childNodes;
       for (var i=0; i<childs.length; i++) {
         if (childs[i].nodeType == 1) {
-          childs[i].style.color = '#000';
+          childs[i].style.color = '';
         }
       }        
     }
   }
 }
-window.onload = closeClick();
+addLoadEvent(closeClick);
+
+// button of 'search', add an action to change color if mouse over it
+function changeColor () {
+  var searchEle = document.getElementById('search-button');
+  searchEle.onmouseover = function() {
+    searchEle.style.backgroundColor = '#ed4259';
+  }
+  searchEle.onmouseout = function() {
+    searchEle.style.backgroundColor = '';
+  }
+}
+addLoadEvent(changeColor);
+
+//  onmouse Event change color, game
+function changeGameColor () {
+  var eles = document.getElementsByClassName('game-hover');
+  for (var i=0; i<eles.length; i++) {
+    eles[i].onmouseover = function() {
+      var that = this;
+      var icons = that.childNodes;
+      for (var j=0; j<icons.length; j++) {
+        if (icons[j].nodeType == 1) {
+          that.style.color = '#ffeea8';
+          icons[j].style.color = '#ffeea8';
+          console.log(icons[j]);
+        }
+      }
+    }
+    eles[i].onmouseout = function() {
+      var that = this;
+      var icons = that.childNodes;
+      for (var j=0; j<icons.length; j++) {
+        if (icons[j].nodeType == 1) {
+          that.style.color = '';
+          icons[j].style.color = '';
+          console.log(icons[j]);
+        }
+      }
+    }
+  }
+}
+addLoadEvent(changeGameColor);

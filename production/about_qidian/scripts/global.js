7aef790a4e483617dcd6f3134e194fbb601f6097
Mingfei Wu <31245615+wmf1993@users.noreply.github.com>
2018-10-01 23:39:54 +0800

    qidian
    continue,

--- production/about_qidian/scripts/global.js
+++ production/about_qidian/scripts/global.js
@@ -119,107 +119,113 @@ addLoadEvent(changeGameColor);
 
 
 /*  === carousel === */
-function moveElement(elemID, move_x) { /*元素ID以及X移动距离 */
+function moveElement(elemID, derection, move_x) { /*元素ID以及X移动方向及距离 */
   var elem = document.getElementById(elemID);
-  
+  elem.style.display = 'list-item';
   elem.style.transitionProperty = 'transform';
   elem.style.transitionDuration = '300ms';
   elem.style.transitionTimingFunction = 'linear';
-  var moveX = 'translateX(' + move_x + '%)';
+  var moveX = 'translateX(' + derection*move_x + '%)';
   elem.style.transform = moveX;
 }
 
 function clickImg() {
   var clickChilds = document.getElementsByClassName('click')[0].childNodes;
   var childsArray=new Array();
+  var that;
   /* -- 对于两边的button，循环调用函数,直至左边或右边兄弟img都挪开 --*/
   function movePrevious(elem){
     if (!elem.previousElementSibling) return;
-    fontElemID = elem.previousElementSibling.href.replace(/.*#(.*)/g,'$1');
-    moveElement(fontElemID, -100);
+    previousElemID = elem.previousElementSibling.href.replace(/.*#(.*)/g,'$1');
+    moveElement(previousElemID, 1, 100);
     movePrevious(elem.previousElementSibling);
   }
   function moveNext(elem) {
     if (!elem.nextElementSibling) return;
       nextElemID = elem.nextElementSibling.href.replace(/.*#(.*)/g,'$1');
-      moveElement(nextElemID, 100);
+      moveElement(nextElemID, -1, 100);
       moveNext(elem.nextElementSibling);
   }
-  
+  /* -- get the elements name of 'a' and convert to Array -- */
   for (var i=0; i<clickChilds.length; i++) {
-    if (clickChilds[i].nodeType == 1) {
-      childsArray.push(clickChilds[i]); // childsArray ???
-      /* -- question: can't set time ==*/
- //     console.log(clickChilds[i].clicked);
-  //    if (clickChilds[i].style.stranform != 'translateX(0%)') clearTimeout(clickChilds[i].clicked);
-      clickChilds[i].onclick = function() {
-        that = this;
-        that.setAttribute('class', 'active');
-        var elemID = that.href.replace(/.*#(.*)/g,'$1');
-        var fontElemID;
-        var nextElemID;
-        moveElement(elemID, 0);
-        clearTimeout(that.clickTimer);  //clear ths clickTimer;
-        
-        if (!that.nextElementSibling) {
-          fontElemID = that.previousElementSibling.href.replace(/.*#(.*)/g,'$1');
-          moveElement(fontElemID, -100);
-          movePrevious(that);
-        }
-        if (!that.previousElementSibling) {
-          nextElemID = that.nextElementSibling.href.replace(/.*#(.*)/g,'$1');
-          moveElement(nextElemID, 100);
-          moveNext(that);
-        }
-        if (that.nextElementSibling && that.previousElementSibling) {
-          fontElemID = that.previousElementSibling.href.replace(/.*#(.*)/g,'$1');
-          nextElemID = that.nextElementSibling.href.replace(/.*#(.*)/g,'$1');
-          moveElement(fontElemID, -100);
-          moveElement(nextElemID, 100);
-        }
-        that.clickTimer = setTimeout(function(){  //clickTimer
+    if (!clickChilds[i].nodeType == 1) continue;
+    if (!clickChilds[i].href) continue;
+    childsArray.push(clickChilds[i]);
+  }
+  
+  /* -- automove -- */
+  for (var i=0; i<childsArray.length; i++) {
+    childsArray[i].onclick = function(e) {
+      that = this;
+      var attribute = 'cycle-button';
+      for (var j=0; j<childsArray.length; j++) {
+        childsArray[j].setAttribute('class', attribute);
+      }
+      attribute += ' active';
+      that.setAttribute('class', attribute);
+      
+      var elemID = that.href.replace(/.*#(.*)/g,'$1');
+      var previousElemID;
+      var nextElemID;
+      moveElement(elemID, -1, 0);
+      
+      for (var j=0; j<childsArray.length; j++) {
+        if (childsArray[j].clickTimer) clearTimeout(childsArray[j].clickTimer);
+      }
+      if (!that.nextElementSibling) {
+        previousElemID = that.previousElementSibling.href.replace(/.*#(.*)/g,'$1');
+        moveElement(previousElemID, -1, 100);
+        nextElemID = childsArray[0].href.replace(/.*#(.*)/g,'$1');
+        moveElement(nextElemID, 1, 100);
+        movePrevious(that);
+      }
+      if (!that.previousElementSibling) {
+        nextElemID = that.nextElementSibling.href.replace(/.*#(.*)/g,'$1');
+        moveElement(nextElemID, -1, 100);
+        previousElemID = childsArray[childsArray.length-1].href.replace(/.*#(.*)/g, '$1');
+        moveElement(previousElemID, -1, 100);
+        moveNext(that);
+      }
+      if (that.nextElementSibling && that.previousElementSibling) {
+        movePrevious(that);
+        moveNext(that);
+      }
+      if (!e.clientX == 0) {
+        return false;
+      } else {
+        that.clickTimer = setTimeout(function(){
           if(!that.nextElementSibling) {
-            clickChilds[1].click();
+            childsArray[0].click();
           } else {
             that.nextElementSibling.click();
           }
-          }, 500);
-       return false; 
+        }, 3000);
+      return false; 
       }
     }
   }
+  
+  childsArray[0].click();
+  
   /* -- addEventListener judge whether click or mouse over or out --*/
-  var clickChild;
   for (var i=0; i<childsArray.length; i++) {
-    childsArray[i].addEventListener('click',function(){ // click
-      clickChild = this;
-      if (this.clickTimer) {
-        if (this.previousElementSibling) {
-          clearTimeout(this.previousElementSibling.clickTimer);
-        } else {
-          clearTimeout(childsArray[childsArray.length-1].clickTimer);
-        }
-      }
-    }, true);
-    childsArray[i].addEventListener('mouseover', function(){ // mouseover
+    childsArray[i].onmouseenter = function(){
       for (var j=0; j<childsArray.length; j++) {
-        if (!childsArray[j].clickTimer && !childsArray[j].resetTimer) continue;
         if (childsArray[j].clickTimer) clearTimeout(childsArray[j].clickTimer);
         if (childsArray[j].resetTimer) clearTimeout(childsArray[j].resetTimer);
       }
-      console.log('over');
-    }, false);
-    childsArray[i].addEventListener('mouseout', function(){ // mouseout
-      clickChild.resetTimer = setTimeout(function(){  //clickTimer for mouseout
-          if(!clickChild.nextElementSibling) {
-            clickChilds[1].click();
-          } else {
-            clickChild.nextElementSibling.click();
-          }
-          }, 500);;
-    }, true);
+    }
+    childsArray[i].onmouseleave = function(){
+      clearTimeout(that.clickTimer);
+      that.clickTimer = setTimeout(function(){  //clickTimer for mouseout
+        if(!that.nextElementSibling) {
+          childsArray[0].click();
+        } else {
+          that.nextElementSibling.click();
+        }
+      }, 3000);
+    }
   }
-  clickChilds[1].click();
 }
 addLoadEvent(clickImg);
 //carousel('item1', '-100%', 300ms)
